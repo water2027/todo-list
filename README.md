a8efcfffd702a7d4b1cd13e47234b0b0f0e22f92
water <linshy76@mail2.sysu.edu.cn>
2025-05-27 23:35:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# 一个简单的TODO List
+
+总而言之先写一个简单的TODO List吧
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'virtual:uno.css'
+import '@/assets/main.scss'
+
+createApp(App).mount('#app')
